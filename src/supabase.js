36cc194679f95c8fd292002b52da3d37541a5630
Marlon <marlon_menezes@outlook.com>
2024-08-14 21:25:54 -0300

    Fix: ajuste no arquivo supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://oddofnuqjknqvairlkhj.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kZG9mbnVxamtucXZhaXJsa2hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM1ODgyMDIsImV4cCI6MjAzOTE2NDIwMn0.DDfPIdhOt_N_kNeLihGU83Rx9YR9dTbTzvAxNOdzZC0"
-// const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
