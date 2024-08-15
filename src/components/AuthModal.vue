87fad215ab5e4568aad990def786f912f3e81f9b
Marlon <marlon_menezes@outlook.com>
2024-08-14 21:24:52 -0300

    Feat: Adicionado validação para os campos de Sign Up

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,30 +1,44 @@
 <script setup>
-import { ref } from 'vue';
+import { storeToRefs } from "pinia";
+import { reactive, ref } from 'vue';
+import { useUserStore } from "../stores/users";
+
+const userStore = useUserStore()
+
+const { errorMessage } = storeToRefs(userStore); // Aqui usamos o storeToRefs para trazer mudança de estado para a mensagem de erro
 
 const props = defineProps(['isLogin']);
-
 const open = ref(false);
+
+// Aqui criaremos as credenciais do usuário em modo reativo, criando um objeto reativo.
+const userCredentials = reactive({
+  email: "",
+  password: "",
+  username: ""
+})
 
 const showModal = () => {
   open.value = true;
 };
 
-const handleOk = e => {
-  open.value = false;
+const handleOk = (e) => {
+  console.log("ok!!!")
+  userStore.handleSignup(userCredentials)
 };
 
 const title = props.isLogin ? 'Login' : 'SignUp';
 
 </script>
 
-<template>
+<template>  
   <div>
     <AButton type="primary" @click="showModal" class="btn">{{title}}</AButton>
     <AModal v-model:open="open" :title="title" @ok="handleOk">
       <div class="modal-content">
-        <AInput v-if="!isLogin" v-model:value="value" placeholder="Username" />
-        <AInput v-model:value="value" placeholder="E-mail" />
-        <AInput v-model:value="value" placeholder="Password" />
+        <AInput v-if="!isLogin" v-model:value="userCredentials.username" placeholder="Username" />
+        <AInput v-model:value="userCredentials.email" placeholder="E-mail" />
+        <AInput v-model:value="userCredentials.password" placeholder="Password" type="password"/>
+        <ATypographyText v-if="errorMessage" type="danger">{{errorMessage}}</ATypographyText>
       </div>
     </AModal>
   </div>
---
+++ src/stores/users.js
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUserStore = defineStore('users', () => {
+  const user = ref(null)
+  const errorMessage = ref("")
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  const handleLogin = () =>{}
+
+  const handleSignup = (credentials) =>{
+    const {email, password, username} = credentials
+
+    if(password.length < 6){
+      return errorMessage.value = "Password length is too short"
+    }
+
+    if(username.length < 4){
+      return errorMessage.value = "Username length is too short"
+    }
+
+    if(!validateEmail(email)){
+      return errorMessage.value = "Email is invalid"
+    }
+
+    errorMessage.value = ""
+  }
+
+  const handleLogout = () =>{}
+
+  const getUser = () =>{}
+
+  return { user, errorMessage, handleLogin, handleSignup, handleLogout, getUser }
+})
